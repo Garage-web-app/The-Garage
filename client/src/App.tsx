be8009ac0f02aac826301389ead858fbf04807b6
Omid Khodaparast <omidkhoda0101@gmail.com>
2025-07-04 14:05:48 +0200

    #36 Add templates for issues and pull requests and set front-end up

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <p className='text-3xl text-red-600'>Count: {count}</p>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    </>
+  )
+}
+
+export default App
