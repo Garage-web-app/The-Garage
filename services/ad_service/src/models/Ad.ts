510aa7eb57217fd547fb186262f33c1549ddd2b0
Makan Ajdadi Salekdeh <gusajdama@student.gu.se>
2025-07-13 22:29:17 +0200

    #36 Add comments and foreign keys for ad model

--- services/ad_service/src/models/Ad.ts
+++ services/ad_service/src/models/Ad.ts
@@ -4,29 +4,35 @@ interface Ad {
   productionYear: number;
   price: number;
   plateNumber: string;
-  picture: string;
-  model: string; // this is fine now
+  picture: string;                                              //String is the picture's url
+  model: string;
   location: {
     country: string;
     city: string;
     street: string;
     zipCode: number;
   };
+  userEmail: string;                                            //userEmail is the foreign key to the user which creates the add
+  adCreationDate: Date;
 }
 
 const adSchema = new Schema<Ad>({
   productionYear: { type: Number, required: true },
   price: { type: Number, required: true },
-  plateNumber: { type: String, required: true, unique: true },
-  picture: { type: String, required: true },
-  model: { type: String, required: true }, // no conflict now
+  plateNumber: { type: String, required: true, unique: true }, //plateNumber is not the key for the add but it is unique
+  picture: { type: String, required: true },                   //String is the picture's url
+  model: { type: String, required: true }, 
   location: {
     country: { type: String, required: true },
     city: { type: String, required: true },
     street: { type: String, required: true },
     zipCode: { type: Number, required: true },
+    required: true
   },
+  userEmail: { type: String, required: true },                //userEmail is the foreign key to the user which creates the add
+  adCreationDate: { type: Date, required: true, default: Date.now }
 });
+
 
 const AdModel = model<Ad>('Ad', adSchema);
 export default AdModel;
